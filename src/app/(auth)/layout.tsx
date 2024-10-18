fb557107d042095b91017dd002fa116996d3a702
Berkan Ümütlü <berkanumutlu1@gmail.com>
2024-10-18 19:25:29 +0300

    Added sign-in, sign-up pages for clerk

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="h-full bg-red-500">
-            Auth Layout
+        <div className="h-full flex flex-col items-center justify-center">
             {children}
         </div>
     );
