97a1db164bfbf62255ff6d2d960912be0d9c7245
Berkan Ümütlü <berkanumutlu1@gmail.com>
2024-10-22 16:32:01 +0300

    Added server edit modal

---
+++ src/app/api/servers/[serverId]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+        const profile = await currentProfile();
+        if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+        const { name, imageUrl } = await req.json();
+        const server = await db.server.update({
+            where: { id: params.serverId, profileId: profile.id },
+            data: { name, imageUrl }
+        });
+        return NextResponse.json(server);
+    } catch (err) {
+        console.log("[SERVER_ID_PATCH]", err);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
